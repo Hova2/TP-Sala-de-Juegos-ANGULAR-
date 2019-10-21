c3dd2431c9486f12fcb72f14ada4688e05eae96c
Juani <nacho.mone@gmail.com>
2019-10-21 13:21:02 -0300

    2019-10-21-09

--- src/app/componentes/mapa-de-google/mapa-de-google.component.ts
+++ src/app/componentes/mapa-de-google/mapa-de-google.component.ts
@@ -9,22 +9,10 @@ export class MapaDeGoogleComponent implements OnInit {
   title: string = 'El Primer mapa';
   lat: number = 33.678418;
   lng: number = 5.809007;
-   // google maps zoom level
-   zoom: number = 8;
-   
+  // google maps zoom level
+  zoom: number = 8;
 
+  constructor() {}
 
-
-  constructor() { }
-  
-  ngOnInit() {
-  }
-
-
-  
-
-
-  
-
+  ngOnInit() {}
 }
-
